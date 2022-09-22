8f9603ff4871e8d5b80be39472bdeee5bf231d5f
MariusDevelops <brazasmarius@gmail.com>
2022-09-22 23:46:36 +0200

    feat: single round

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,30 @@
-// begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// 1. Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
-  const words = ["Rock", "Paper", "Scissors"];
+  const words = ["rock", "paper", "scissors"];
   let word = words[Math.floor(Math.random() * words.length)];
-  return console.log(word);
+  return word;
 }
 
 getComputerChoice();
+
+// 2. Write a function that plays a single round of Rock Paper Scissors.
+// The function should take two parameters - the playerSelection and
+// computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return console.log("you won");
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return console.log("you lost");
+  } else {
+    return console.log("draw");
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log("player: " + playerSelection);
+console.log("computer: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+// 3. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
