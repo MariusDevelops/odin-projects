44ae81ae08404ec50ba06f1d0524ae91f0028a8e
MariusDevelops <brazasmarius@gmail.com>
2022-09-24 23:16:00 +0200

    feat: five round game

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -12,19 +12,52 @@ getComputerChoice();
 // computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "scissors") {
-    return console.log("you won");
+    return "winner: player wins!";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return console.log("you lost");
+    return "winner: computer wins!";
   } else {
-    return console.log("draw");
+    return "winner: tie! nobody wins!";
   }
 }
 
 // 3. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-let guess = "RoCk";
-const playerSelection = guess.toLowerCase();
-const computerSelection = getComputerChoice();
+// let guess = "ROCK";
+// const playerSelection = guess.toLowerCase();
+// const computerSelection = getComputerChoice();
 
-console.log("player: " + playerSelection);
-console.log("computer: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// console.log("player: " + playerSelection);
+// console.log("computer: " + computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+// 4. Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game
+// that keeps score and reports a winner or loser at the end.
+function game() {
+  let score = 0;
+
+  for (let i = 0; i < 5; i++) {
+    // 3. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+    let guess = "ROCK";
+    const playerSelection = guess.toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    // if (
+    //   playRound(playerSelection, computerSelection) === "winner: player wins!"
+    // ) {
+    //   score = score + 1;
+    // } else if (
+    //   playRound(playerSelection, computerSelection) === "winner: computer wins!"
+    // ) {
+    //   score = score - 1;
+    // } else {
+    //   score = score;
+    // }
+
+    console.log("player: " + playerSelection);
+    console.log("computer: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    // console.log(score);
+    console.log("//////////////////");
+  }
+}
+
+game();
