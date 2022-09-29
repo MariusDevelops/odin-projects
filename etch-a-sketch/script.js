ea62acee077ac84aabde09f2accb44051c1a4653
MariusDevelops <brazasmarius@gmail.com>
2022-09-30 00:10:40 +0200

    feat: 16x16 grid

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -8,8 +8,26 @@ heading.textContent = "Etch A Sketch";
 document.body.appendChild(heading);
 
 // Create a webpage with a 16x16 grid of square divs.
-const div1 = document.createElement("div");
-document.body.appendChild(div1);
+const div = document.createElement("div");
+div.style.width = "864px";
+div.style.height = "864px";
+div.style.display = "flex";
+div.style.flexWrap = "wrap";
+document.body.appendChild(div);
 
-const div2 = document.createElement("div");
-div1.appendChild(div2);
+// const div2 = document.createElement("div");
+// div2.style.width = "16px";
+// div2.style.height = "16px";
+// div2.style.border = "1px solid black";
+// div1.appendChild(div2);
+
+function grid() {
+  for (i = 0; i < 256; i++) {
+    const div2 = document.createElement("div");
+    div2.style.width = "50px";
+    div2.style.height = "50px";
+    div2.style.border = "2px solid black";
+    div.appendChild(div2);
+  }
+}
+grid();
